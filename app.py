7ed3612de8177981eee8f8207dacd86554dc9ea4
deepakawsm24-bit <deepakawsm24@gmail.com>
2026-06-01 22:28:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ address = st.text_area("Address")
 department = st.selectbox("Department",["Cardiology","Orthopedic","Neurology","General Medicine","Pediatrics"])
 visit_type = st.radio("Visit Type",["OPD","IPD"])
 if st.button("Register Patient"):
-    cursor.execute(" INSERT INTO patients VALUES
-(?,?,?,?,?,?,?,?,?)",
+    cursor.execute( "INSERT INTO patients VALUES
+(?,?,?,?,?,?,?,?,?,?)",
     (
         patient_id,
         str(name),
