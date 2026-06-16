37230bc584cba1820ecaf1099f4bef1f2aa21bc2
deepakawsm24-bit <deepakawsm24@gmail.com>
2026-06-16 20:10:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ if menu =="Dashboard":
     st.subheader("Hospital Dashboard")
     total_patients = cursor.execute("SELECT COUNT(*) FROM patients").fetchone()[0]
     st.metric("Total Patients",total_patients)
+    male_patients = cursor.execute("SELECT COUNT(*) FROM patients WHERE gender='Male'").fetchone()[0]
     female_patients = cursor.execute("SELECT COUNT(*) FROM patients WHERE gender ='Female'").fetchone()[0]
     col1, col2, col3 = st.columns(3)
     with col1:
