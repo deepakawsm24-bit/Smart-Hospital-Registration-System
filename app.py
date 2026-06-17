8c8fb61f1d73ded67ef7034915dc829fc1adc392
deepakawsm24-bit <deepakawsm24@gmail.com>
2026-06-17 20:06:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if menu =="Dashboard":
         st.subheader("Department Wise Patients")
         st.bar_chart(dept_df.set_index("Department"))
 else:
-    st.info("No department data available")
+        st.info("No department data available")
         
 if menu == "New Registration":
     patient_id = "PAT" + str(random.randint(1000,9999))
